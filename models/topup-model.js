fb623dd7a1f50e5123817f42808a8219cf33fa4b
nutsang <foxteary@gmail.com>
2024-03-01 04:38:50 +0700

    5.1.0

--- models/topup-model.js
+++ models/topup-model.js
@@ -12,11 +12,11 @@ module.exports.topUp = (request, response) => {
             const tw = await twApi(requestGiftTrueMoney, process.env.PHONEVOUCHER)
             if(tw.status.code == 'SUCCESS'){
                 const baht = tw.data.my_ticket.amount_baht
-                connection.query('UPDATE finance SET cash_amount = cash_amount + ?, aysel_amount = aysel_amount + ?, update_at = ? WHERE email = ?', [baht, baht*process.env.AYSEL, new Date(), requestEmail], (error, result) => {
+                connection.query('UPDATE finance SET cash_amount = cash_amount + ?, aysel_amount = aysel_amount + ?, update_at = ? WHERE email = ?', [baht, (process.env.AYSEL/process.env.BAHT)*baht, new Date(), requestEmail], (error, result) => {
                     if(error){
                         response.status(200).json({status: false, payload: 'เติม Aysel ล้มเหลว'})
                     }else{
-                        connection.query('INSERT INTO history_payment (uuid, email, aysel_amount, cash_amount, payment_status, create_at, update_at) VALUES (?, ?, ?, ?, ?, ?, ?)', [uuid.v4(), requestEmail, baht*process.env.AYSEL, baht, true, new Date(), new Date()], (error, result) => {
+                        connection.query('INSERT INTO history_payment (uuid, email, aysel_amount, cash_amount, payment_status, create_at, update_at) VALUES (?, ?, ?, ?, ?, ?, ?)', [uuid.v4(), requestEmail, (process.env.AYSEL/process.env.BAHT)*baht, baht, true, new Date(), new Date()], (error, result) => {
                             if(error){
                                 response.status(200).json({status: false, payload: 'เติม Aysel ล้มเหลว'})
                             }else{
