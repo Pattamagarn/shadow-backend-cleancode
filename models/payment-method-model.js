4ba15fa6ed52fb674f8717bc5771e5e242ab914a
pattamagarn <pattamagarn.o@gmail.com>
2024-03-28 20:34:01 +0700

    0.0.0

--- models/payment-method-model.js
+++ models/payment-method-model.js
@@ -23,9 +23,9 @@ const storagePaymentMethod = multer.diskStorage({
 const upload = multer({
     storage: storagePaymentMethod,
     fileFilter: (request, file, callback) => {
-        if(file.mimetype === 'image/png'){
+        if (file.mimetype === 'image/png') {
             callback(null, true)
-        }else{
+        } else {
             callback(new Error('ใช้ได้แค่ไฟล์ .png เท่านั้น'), false)
         }
     }
@@ -33,42 +33,50 @@ const upload = multer({
 
 module.exports.paymentMethodSelect = (request, response) => {
     connection.query('SELECT uuid, method, information, create_at, update_at FROM payment_method', [], (error, result) => {
-        if(error){
-            response.status(200).json({status: false, payload: error})
-        }else{
-            response.status(200).json({status: true, payload: result})
+        if (error) {
+            response.status(200).json({ status: false, payload: error })
+        } else {
+            response.status(200).json({ status: true, payload: result })
         }
     })
 }
 
 module.exports.paymentMethodUpdateImage = (request, response) => {
     upload.single('file')(request, response, (error) => {
-        if(error){
-            response.status(200).json({status: false, payload: `${error} ใช้ได้แค่ไฟล์ .png เท่านั้น`})
-        }else{
-            try{
-                const token = request.cookies.token
-                jsonwebtoken.verify(token, SECRET)
+        if (error) {
+            response.status(200).json({ status: false, payload: `${error} ใช้ได้แค่ไฟล์ .png เท่านั้น` })
+        } else {
+            try {
+                // const token = request.cookies.token
+                // jsonwebtoken.verify(token, SECRET)
                 const requestUUID = request.body.uuid
                 const requestInformation = request.file.filename
                 connection.query('SELECT uuid, method, information, create_at, update_at FROM payment_method WHERE uuid = ?', [requestUUID], (error, result) => {
-                    if(error){
-                        response.status(200).json({status: false, payload: 'แก้ไขล้มเหลว'})
-                    }else{
+                    if (error) {
+                        response.status(200).json({ status: false, payload: 'แก้ไขล้มเหลว' })
+                    } else {
                         const information = result[0].information
-                        fs.unlinkSync(path.join('/public/images/payment-method', information))
+                        console.log(information)
+                        // fs.unlinkSync(path.join('/public/images/payment-method', information))
                         connection.query('UPDATE payment_method SET information = ?, update_at = ? WHERE uuid = ?', [requestInformation, new Date(), requestUUID], (error, result) => {
-                            if(error){
-                                response.status(200).json({status: false, payload: 'แก้ไขล้มเหลว'})
-                            }else{
-                                response.status(200).json({status: true, payload: 'แก้ไขสำเร็จ'})
+                            if (error) {
+                                fs.unlinkSync(path.join('/public/images/payment-method', request.file.filename))
+                                response.status(200).json({ status: false, payload: 'แก้ไขล้มเหลว' })
+                            } else {
+                                response.status(200).json({ status: true, payload: 'แก้ไขสำเร็จ' })
                             }
                         })
                     }
                 })
-            }catch(error){
-                fs.unlinkSync(path.join('/public/images/payment-method', request.file.filename))
-                response.status(200).json({status: false, payload: 'แก้ไขล้มเหลว'})
+            } catch (error) {
+                try {
+                    fs.unlinkSync(path.join('/public/images/payment-method', request.file.filename))
+                    response.status(200).json({ status: false, payload: 'แก้ไขภาพวิธีชำระเงินล้มเหลว' })
+                }
+                catch(error){
+                    response.status(200).json({status: false, payload: 'แก้ไขภาพวิธีชำระเงินล้มเหลว'})
+                }
+                
             }
         }
     })
@@ -78,10 +86,10 @@ module.exports.paymentMethodUpdateVideo = (request, response) => {
     const requestUUID = request.body.uuid
     const requestInformation = request.body.information
     connection.query('UPDATE payment_method SET information = ?, update_at = ? WHERE uuid = ?', [requestInformation, new Date(), requestUUID], (error, result) => {
-        if(error){
-            response.status(200).json({status: false, payload: 'แก้ไขล้มเหลว'})
-        }else{
-            response.status(200).json({status: true, payload: 'แก้ไขสำเร็จ'})
+        if (error) {
+            response.status(200).json({ status: false, payload: 'แก้ไขล้มเหลว' })
+        } else {
+            response.status(200).json({ status: true, payload: 'แก้ไขสำเร็จ' })
         }
     })
 }
@@ -89,16 +97,16 @@ module.exports.paymentMethodUpdateVideo = (request, response) => {
 module.exports.deletePaymentMethodImage = (request, response) => {
     const requestUUID = request.params.uuid
     connection.query('SELECT information FROM payment_method WHERE uuid = ?', [requestUUID], (error, result) => {
-        if(error){
-            response.status(200).json({status: false, payload: 'ลบ Payment Method Image ล้มเหลว'})
-        }else{
+        if (error) {
+            response.status(200).json({ status: false, payload: 'ลบ Payment Method Image ล้มเหลว' })
+        } else {
             const information = result[0].information
             connection.query("UPDATE payment_method SET information = '' WHERE uuid = ?", [requestUUID], (error, result) => {
-                if(error){
-                    response.status(200).json({status: false, payload: 'ลบ Payment Method Image ล้มเหลว'})
-                }else{
+                if (error) {
+                    response.status(200).json({ status: false, payload: 'ลบ Payment Method Image ล้มเหลว' })
+                } else {
                     fs.unlinkSync(path.join('./public/images/payment_method', information))
-                    response.status(200).json({status: true, payload: 'ลบ Payment Method Image สำเร็จ'})
+                    response.status(200).json({ status: true, payload: 'ลบ Payment Method Image สำเร็จ' })
                 }
             })
         }
@@ -108,14 +116,14 @@ module.exports.deletePaymentMethodImage = (request, response) => {
 module.exports.deletePaymentMethodVideo = (request, response) => {
     const requestUUID = request.params.uuid
     connection.query('SELECT information FROM payment_method WHERE uuid = ?', [requestUUID], (error, result) => {
-        if(error){
-            response.status(200).json({status: false, payload: 'ลบ Payment Method video ล้มเหลว'})
-        }else{
+        if (error) {
+            response.status(200).json({ status: false, payload: 'ลบ Payment Method video ล้มเหลว' })
+        } else {
             connection.query("UPDATE payment_method SET information = '' WHERE uuid = ?", [requestUUID], (error, result) => {
-                if(error){
-                    response.status(200).json({status: false, payload: 'ลบ Payment Method video ล้มเหลว'})
-                }else{
-                    response.status(200).json({status: true, payload: 'ลบ Payment Method video สำเร็จ'})
+                if (error) {
+                    response.status(200).json({ status: false, payload: 'ลบ Payment Method video ล้มเหลว' })
+                } else {
+                    response.status(200).json({ status: true, payload: 'ลบ Payment Method video สำเร็จ' })
                 }
             })
         }
